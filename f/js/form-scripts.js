ef9c119de0d98975920512076a5ecf756eb3af9a
git <smendoza.barrera@gmail.com>
2015-10-20 11:15:57 -0500

    Form integration v0.49.40

--- f/js/form-scripts.js
+++ f/js/form-scripts.js
@@ -47,7 +47,7 @@ function formError(){
 function submitMSG(valid, msg){
     if(valid){
         // var msgClasses = "h3 text-center tada animated text-success";
-        var msgClasses = "h4 text-center animated bg-info";
+        var msgClasses = "h4 text-center animated text-warning";
     } else {
         // var msgClasses = "h3 text-center text-danger";
         var msgClasses = "h4 text-center text-warning";
