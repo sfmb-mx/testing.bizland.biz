c803947593b32a8742d7b67d52a2f942d39459ec
git <smendoza.barrera@gmail.com>
2015-12-23 10:45:20 -0600

    Yahuiche community attractions toggle text done v0.0.48

--- javascripts/custom/custom-toggle.js
+++ javascripts/custom/custom-toggle.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
-    $("#tButton").click(function(){
-        $("#tText1").toggle(1000);
+    $("#tButton1").click(function(){
+        $("#tText1").toggle(400);
+    });
+});
+
+$(document).ready(function(){
+    $("#tButton2").click(function(){
+        $("#tText2").toggle(400);
+    });
+});
+
+$(document).ready(function(){
+    $("#tButton3").click(function(){
+        $("#tText3").toggle(400);
+    });
+});
+
+$(document).ready(function(){
+    $("#tButton4").click(function(){
+        $("#tText4").toggle(400);
+    });
+});
+
+$(document).ready(function(){
+    $("#tButton5").click(function(){
+        $("#tText5").toggle(400);
+    });
+});
+
+$(document).ready(function(){
+    $("#tButton6").click(function(){
+        $("#tText6").toggle(400);
     });
 });
